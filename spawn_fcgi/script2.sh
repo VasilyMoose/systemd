88b3e0e180f43d07ac6ea37f1bac147696c0ca34
VasilyMoose <96911395+VasilyMoose@users.noreply.github.com>
2023-02-24 12:39:58 +0300

    Update script2.sh

--- spawn_fcgi/script2.sh
+++ spawn_fcgi/script2.sh
@@ -1,35 +1,4 @@
 yum install epel-release -y && yum install spawn-fcgi php php-climod_fcgid httpd -y
-rm -rf /etc/sysconfig/spawn-fcgi
-cd /etc/sysconfig/
 
-cat << EOF > spawn-fcgi
-# You must set some working options before the "spawn-fcgi" service will work.
-# If SOCKET points to a file, then this file is cleaned up by the init script.
-#
-# See spawn-fcgi(1) for all possible options.
-#
-# Example :
-SOCKET=/var/run/php-fcgi.sock
-OPTIONS="-u apache -g apache -s $SOCKET -S -M 0600 -C 32 -F 1 -P /var/run/spawn-fcgi.pid -- /usr/bin/php-cgi"
-EOF
-
-
-cd /etc/systemd/system/
-
-cat << EOF > spawn-fcgi.service
-[Unit]
-Description=Spawn-fcgi startup service by Otus
-After=network.target
-
-[Service]
-Type=simple
-PIDFile=/var/run/spawn-fcgi.pid
-EnvironmentFile=/etc/sysconfig/spawn-fcgi
-ExecStart=/usr/bin/spawn-fcgi -n $OPTIONS
-KillMode=process
-
-[Install]
-WantedBy=multi-user.target
-EOF
 systemctl daemon-reload
 systemctl start spawn-fcgi
