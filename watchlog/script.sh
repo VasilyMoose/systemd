1cde310b185345a6e5c67e8f90d293422e160e14
VasilyMoose <96911395+VasilyMoose@users.noreply.github.com>
2023-02-24 15:39:05 +0300

    Update script.sh

--- watchlog/script.sh
+++ watchlog/script.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 mv watchlog /var/log/
 cd /etc/sysconfig/
+mv watchlog.sh /opt/
+chmod +x /opt/watchlog.sh
+ 
+mv watchlog.timer /etc/systemd/system/
+mv watchlog.service /etc/systemd/system/
 
 systemctl daemon-reload
 systemctl start watchlog.timer
