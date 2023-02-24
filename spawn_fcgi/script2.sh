a9eb9ad1db1b4e252b919db55dac31dad30da629
VasilyMoose <96911395+VasilyMoose@users.noreply.github.com>
2023-02-24 17:14:11 +0300

    Update script2.sh

--- spawn_fcgi/script2.sh
+++ spawn_fcgi/script2.sh
@@ -1,4 +1,8 @@
 yum install epel-release -y && yum install spawn-fcgi php php-climod_fcgid httpd -y
 
+rm -rf /etc/sysconfig/spawn-fcgi
+mv /root/systemd/spawn_fcgi/spawn-fcgi /etc/sysconfig/
+mv /root/systemd/spawn_fcgi/spawn-fcgi.service /etc/systemd/system/
+
 systemctl daemon-reload
 systemctl start spawn-fcgi
