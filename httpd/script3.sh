fb40a0724f74b4516dc6c55c628f67222cd6d1f7
VasilyMoose <96911395+VasilyMoose@users.noreply.github.com>
2023-02-24 23:21:46 +0300

    Update script3.sh

--- httpd/script3.sh
+++ httpd/script3.sh
@@ -3,5 +3,6 @@ setenforce 0
 sed -i '/Listen 80/d' /etc/httpd/conf/httpd.conf
 cp /root/systemd/httpd/httpd@.service /etc/systemd/system
 cp /root/systemd/httpd/tmp.conf /etc/httpd/conf.d
+cp /root/systemd/httpd/httpd{1,2} /etc/sysconfig
 systemctl enable --now httpd@httpd1.service
 systemctl enable --now httpd@httpd2.service
